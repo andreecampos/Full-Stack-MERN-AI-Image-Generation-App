5c459579c1af9f439612708863629e06f32d6c6b
andreecampos <orlandoandree1998@gmail.com>
2023-11-27 23:17:26 +0100

    HomePage

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormField = () => {
+  return (
+    <div>
+      Form field
+    </div>
+  )
+}
+
+export default FormField
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePost = () => {
   return (
     <div>
-      <h1>CreatePost</h1>
+      Create Post
     </div>
   )
 }
