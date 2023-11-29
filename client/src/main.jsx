64cc1fd744982d0a406421199be1ffa8998e5a63
andreecampos <orlandoandree1998@gmail.com>
2023-11-29 11:15:07 +0100

    CreatePost done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Test from './constants/Test.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* <Test/> */}
+ 
   </React.StrictMode>,
 )
